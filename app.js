3599279b2dbb1a13bec68f9e06c1a4edd93a4b1b
Dolly124 <133591568+Dolly124@users.noreply.github.com>
2023-08-08 10:36:30 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// const pairSelection = Array.from(document.getElementsByName("pairSelection"));
-// const pairSelection = document.getElementsByName("pairSelection");
+
 const pairSelection = document.querySelectorAll(".cart__option__pair-selector"); // Here we Need All selected elements so we need to use querySelectorAll
 
 console.log(pairSelection);
@@ -28,34 +27,3 @@ pairSelection.forEach((element) => {
 });
 
 //===================================================================================
-
-// function displayNone() {
-//   const allCartOptions = document.querySelectorAll(".cart__option");
-
-//   allCartOptions.forEach((element) => {
-//     const isSelected = element.classList.contains("cart__option--selected");
-
-//     if (isSelected) {
-//       element.classList.remove("cart__option--selected");
-//     }
-//   });
-// }
-
-// function onePairr() {
-//   const child = document.getElementById("onePair");
-//   const parent = child.closest(".cart__option");
-//   parent.classList.add(
-//     "cart__option--selected",
-//     ".cart__option--selected>.cart__option__grid-container"
-//   );
-// }
-
-// function twoPairr() {
-//   const child = document.getElementById("twoPair");
-//   const parent = child.closest(".cart__option");
-//   parent.classList.add(
-//     "cart__option--selected",
-//     ".cart__option--selected>.cart__option__grid-container"
-//   );
-//   displayNone();
-// }
